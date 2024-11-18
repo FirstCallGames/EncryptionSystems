b535f162858babacfe8dadac0fbd592010485957
AadityaChaturvedy <51356216+FirstCallGames@users.noreply.github.com>
2024-11-18 14:18:20 +0530

    1. Caesar Cipher
    Caesar Cipher named after Julius Caesar, was used to protect military conversation. This version of the cipher asks user to enter text and file location to encrypt and decrypt it and save the file as well.

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,6 +1,6 @@
 import string
 
-def create_caesar_dict():
+def caesar():
     l = string.ascii_lowercase
     l = list(l)
     Enc_dict = {}
@@ -10,29 +10,60 @@ def create_caesar_dict():
         Enc_dict[l[i]] = l[(i+3) % 26]
         Dec_dict[l[(i+3) % 26]] = l[i]
     
-    action = input("Enter action ('Decode [D]', 'Encode [E]'): ")
-
-    if action == "E":
-        text_input = input("Enter text to Cipher: ")
-    
+    def Encode(text):
         ans_text = ""
-        for i in text_input:
-            if i != " ":
+        for i in text:
+            if i.isalpha():
                 ans_text += Enc_dict[i.lower()]
             else:
-                ans_text += " "
-        print(ans_text)
-
-    elif action == "D":
-        text_input = input("Enter text to Decipher: ")
+                ans_text += i
+        return ans_text
+    
+    def Decode(text):
         ans_text = ""
-        for i in text_input:
-            if i != " ":
+        for i in text:
+            if i.isalpha():
                 ans_text += Dec_dict[i.lower()]
             else:
-                ans_text += " "
-        print(ans_text)
+                ans_text += i
+        return ans_text
 
+    action = input("Enter action ('File [F]', 'Text [T]'): ")
 
-create_caesar_dict()
+    if action.lower() == "t":
+        action = input("Enter action ('Decode [D]', 'Encode [E]'): ")
 
+        if action == "E":
+            text_input = input("Enter text to Cipher: ")
+            print(Encode(text_input))
+
+        elif action == "D":
+            text_input = input("Enter text to Decipher: ")
+            print(Decode(text_input))
+
+    elif action.lower() == "f":
+        file_location = input("Enter the location of file: ")
+        file_save_location = input("Enter location to save file: ")
+        action = input("Enter action ('Decode [D]', 'Encode [E]'): ")
+
+        if action == "E":
+            try:
+                with open(file_location, "r") as file:
+                    text_file = file.read()
+                with open(file_save_location, "w") as file2:
+                    file2.write(Encode(text_file))
+                print("The Ciphered data is saved.")
+            except FileNotFoundError:
+                print("File does not exist.")
+
+        elif action == "D":
+            try:
+                with open(file_location, "r") as file:
+                    text_file = file.read()
+                with open(file_save_location, "w") as file2:
+                    file2.write(Decode(text_file))
+                print("The deciphered data is saved.")
+            except FileNotFoundError:
+                print("File does not exist.")
+
+caesar()
